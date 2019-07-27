43d8c2b1368424da4e443e587198ca914c6df290
djhprojects <53234861+djhprojects@users.noreply.github.com>
2019-07-27 12:26:51 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function epicycles(x, y, rotation, fourier) {
     x += radius * cos(freq * time + phase + rotation);
     y += radius * sin(freq * time + phase + rotation);
 
-    stroke(255, 100);
+    stroke(150, 100);
     noFill();
     ellipse(prevx, prevy, radius * 2);
     stroke(255);
